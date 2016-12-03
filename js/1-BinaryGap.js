efc3f37619fba5d930baca2795eb8358e3dd3b31
munazo <ayu126@gmail.com>
2016-12-03 23:38:40 +0900

    style(js): airbnb styled

--- js/1-BinaryGap.js
+++ js/1-BinaryGap.js
@@ -1,26 +1,25 @@
 // https://codility.com/programmers/lessons/1-iterations/binary_gap/start/
 
-solution = (N) => {
-	let two_dig = parseInt(N, 10).toString(2);
-	let result = 0;
+const solution = (N) => {
+  let twoDig = parseInt(N, 10).toString(2);
+  let result = 0;
 
-	two_dig = two_dig.replace(/0+$/, "");
+  twoDig = twoDig.replace(/0+$/, '');
 
-	candidates = two_dig.split("1").filter((n)=>{
-		if (result < n.length) {
-			result = n.length;
-		}
+  const candidates = twoDig.split('1').filter((n) => {
+    if (result < n.length) {
+      result = n.length;
+    }
 
-		n !== '';
-		return n;
-	});
-	
-	if (candidates.length < 1) {
-		return 0;
-	}
+    return n;
+  });
 
-	return result;
-}
+  if (candidates.length < 1) {
+    return 0;
+  }
 
-s = solution(15);
+  return result;
+};
+
+const s = solution(15);
 console.log(s);
--- js/2-CyclicRotation.js
+++ js/2-CyclicRotation.js
@@ -1,13 +1,13 @@
 // https://codility.com/programmers/lessons/2-arrays/cyclic_rotation/
 
-solution = (A, K) => {
-	if (A.length < 1) { return []; }
-	
-	for(i = 0; i < K % A.length; i++) {
-		A.unshift(A.pop());
-	}
-	
-	return A;
-}
+const solution = (A, K) => {
+  if (A.length < 1) { return []; }
 
-console.log(solution([1,2,3], 4));
+  for (let i = 0; i < K % A.length; i += 1) {
+    A.unshift(A.pop());
+  }
+
+  return A;
+};
+
+console.log(solution([1, 2, 3], 4));
